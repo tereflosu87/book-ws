5dc583690d6bdef68fc4855404c9f342029abd24
tereflosu87 <tereflosu@gmail.com>
2020-05-17 20:07:26 +0200

    Update BookMapper

--- src/main/java/com/ecomerce/mapper/BookMapper.java
+++ src/main/java/com/ecomerce/mapper/BookMapper.java
@@ -3,6 +3,7 @@ package com.ecomerce.mapper;
 import com.ecomerce.dto.Book;
 import com.ecomerce.dto.BookOutDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
